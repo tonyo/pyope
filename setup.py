18bdc7bb7d03dab7780a53638cb686f1ca938e0a
Anton Ovchinnikov <revolver112@gmail.com>
2014-12-29 15:56:16 +0100

    Add initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyope',
+      version='0.0.1',
+      description='Implementation of order-preserving encryption in Python',
+      author='Anton Ovchinnikov',
+      author_email='anton.ovchi2nikov@gmail.com',
+      license='MIT',
+      packages=['pyope'],
+)
